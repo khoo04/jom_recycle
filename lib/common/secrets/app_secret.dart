4a2b397f5ffc9254bb9f4bb8a5ce545dfce9cc1e
khoo04 <khoozhenxian@gmail.com>
2025-03-30 12:26:18 +0800

    Revoked api key

--- lib/common/secrets/app_secret.dart
+++ lib/common/secrets/app_secret.dart
@@ -1,12 +1,11 @@
 class AppSecret {
   //Google Maps Platform API Key
-  static const mapAPIKey = "AIzaSyDEUdUnGjGZrww9E6f4VvApVeX5CRTBC5U";
+  static const mapAPIKey = "<GOOGLE MAP API KEY>";
 
   //Firebase Api Key
-  static const firebaseWebApiKey = "AIzaSyCv30QU1WIdncepFtcTJDCJWeuMHAbMLYc";
-  static const firebaseAndroidApiKey =
-      "AIzaSyAxekpPc85v1gDKl-M_Rai7fJCq1bq69hc";
-  static const firebaseIOSApiKey = "AIzaSyCv30QU1WIdncepFtcTJDCJWeuMHAbMLYc";
+  static const firebaseWebApiKey = "<FIREBASE WEB API KEY>";
+  static const firebaseAndroidApiKey = "<FIREBASE ANDROID API KEY>";
+  static const firebaseIOSApiKey = "<FIREBASE IOS API KEY>";
 
   static const analyzeImageApiKey = "<ANALYZE IMAGE API KEY>";
   //Add Secret Key As Your Needs
